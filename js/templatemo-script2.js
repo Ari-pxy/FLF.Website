1508aa6f9c727f16402b9d4c9069071f4effded7
Kadeskie <146968201+Kadeskie@users.noreply.github.com>
2023-10-17 18:39:26 +0800

    Update templatemo-script2.js

--- js/templatemo-script2.js
+++ js/templatemo-script2.js
@@ -204,4 +204,25 @@ let calcScrollValue = () => {
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
     })
+let calcScrollValue = () => {
+      let scrollProgress = document.getElementById("progress");
+      let progressValue = document.getElementById("progress-value");
+      let pos = document.documentElement.scrollTop;
+      let calcHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      let scrollValue = Math.round((pos * 100) / calcHeight);
+      if (pos > 100) {
+        scrollProgress.style.display = "grid";
+      } else {
+        scrollProgress.style.display = "none";
+      }
+      scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+      });
+      scrollProgress.style.background = `conic-gradient(#8ea5eb,#6667AB,#5E5A80 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+    };
+    window.onscroll = calcScrollValue;
+    window.onload = calcScrollValue;
+        
     
